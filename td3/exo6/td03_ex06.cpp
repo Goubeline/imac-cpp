e44a290285d916b5ed8351e00a50682f1dc070d1
loic <loic.lebray.llb@gmail.com>
2024-09-24 20:36:33 +0200

    td3 sans bonus

--- td3/exo6/td03_ex06.cpp
+++ td3/exo6/td03_ex06.cpp
@@ -1,18 +1,100 @@
 #include <iostream>
+#include <limits>
 
 int main()
 {
-    float const pi {3.141592};
-    float a {pi*3}; // je veux le résultat de pi (environ 3.141592) fois 3
-    int b {8};
+    int forme;
+    do
+    {
+        std::cout << "0 pour triangle rectangle | 1 pour sapin de noël | 2 pour carré : ";
+        std::cin >> forme;
+        if (!std::cin.fail() && std::cin.peek() == '\n' && (forme >= 0 && forme < 3))
+        {
+            break;
+        }
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "La valeur entrée doit être 0, 1 ou 2 \n";
+    } while(true);
 
-    a += 20;  // je veux ajouter le nombre 20 à la valeur de b
+    int x;
+    do
+    {
+        std::cout << "Saisir la hauteur du ";
+        switch (forme)
+        {
+        case 0:
+            std::cout << "triangle : ";
+            break;
+        case 1:
+            std::cout << "sapin : ";
+            break;
+        default:
+            std::cout << "carré : ";
+            break;
+        }
 
-    b = b * 15; // je veux modifier la valeur de b en la multipliant par 15
+        std::cin >> x;
+        if (!std::cin.fail() && std::cin.peek() == '\n' && x >= 0)
+        {
+            break;
+        }
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "La valeur entrée doit être un nombre entier positif \n";
+    } while(true);
+    
+    if (!forme)
+    {
+        for (int i = 1; i <= x; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                std::cout << '*';
+            }
+            std::cout << std::endl;
+        }
+    }
+    else if (forme == 2)
+    {
+        for (int i = 0; i < x; i++)
+        {
+            std::cout << '*';
+        }
+        std::cout << std::endl;
 
-    std::cout << "a: " << a << std::endl;
-
-    std::cout << "b: " << b << std::endl;
-
-    return 0;
+        for (int i = 2; i < x; i++)
+        {
+            std::cout << '*';
+            for (int j = 0; j < x - 2; j++)
+            {
+                std::cout << ' ';
+            }
+            std::cout << '*' << std::endl;
+        }
+        if (x != 1)
+        {
+            for (int i = 0; i < x; i++)
+            {
+                std::cout << '*';
+            }
+            std::cout << std::endl;
+        }
+    }
+    else
+    {
+        for (int i = 1; i <= x; i++)
+        {
+            for (int j = i; j < x; j++)
+            {
+                std::cout << ' ';
+            }
+            for (int j = 0; j < (2*i - 1); j++)
+            {
+                std::cout << '*';
+            }
+            std::cout << std::endl;
+        }
+    }
+    return 0;    
 }
